c998e835c3ba1bf642e22a837083749ccd21fa7e
Afaqhaider20 <afaqbharwana@gmail.com>
2025-05-25 19:38:06 +0500

    allowed cors

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
