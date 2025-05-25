6d26ea065478a1ff774a44406b2d509988fff279
Afaqhaider20 <afaqbharwana@gmail.com>
2025-05-25 19:55:34 +0500

    Refactor CORS middleware in server configuration to use default settings

--- backend/server.js
+++ backend/server.js
@@ -45,12 +45,7 @@ if (!process.env.MONGO_URI) {
 const app = express();
 
 // Middlewares
-app.use(cors({
-    origin: '*',
-    // credentials: true,
-    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    // allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
